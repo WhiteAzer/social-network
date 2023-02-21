fd248ff2be8d6a14d5e98f11fc0a6edff03a758e
brvjeo <preobars@gmail.com>
2023-02-21 13:42:31 +0300

    repo: fill folders

---
+++ src/hooks/useToggle.ts
@@ -0,0 +1,18 @@
+import { useCallback, useState } from 'react';
+
+export function useToggle(initialState: boolean): {
+	state: boolean;
+	toggle: () => void;
+	setTrue: () => void;
+	setFalse: () => void;
+} {
+	const [state, setState] = useState(initialState);
+
+	const toggle = useCallback(() => setState((current) => !current), []);
+
+	const setTrue = useCallback(() => setState(true), []);
+
+	const setFalse = useCallback(() => setState(false), []);
+
+	return { state, toggle, setTrue, setFalse };
+}
