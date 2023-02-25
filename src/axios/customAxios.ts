436baaefead05a652e7f5dd7d75047636f47e523
Temka Artemka <yandex949@gmail.com>
2023-02-25 16:42:55 +0300

    feat: custom axios instanse

---
+++ src/axios/customAxios.ts
@@ -0,0 +1,9 @@
+import axios from 'axios'
+
+export const serverURL = 'http://localhost:2000'
+
+const RESTAPI_URL = serverURL + '/api'
+export const serverAPI = axios.create({
+	baseURL: RESTAPI_URL,
+	withCredentials: true,
+})
