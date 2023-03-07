af02a9da6b2dd2aeea04b3c34bc990023f203bef
WhiteAzer <0202timur2020@gmail.com>
2023-03-07 16:16:56 +0300

    feat: added auth form

--- src/features/authorization/components/AuthorizationForm/AuthorizationForm.tsx
+++ src/features/authorization/components/AuthorizationForm/AuthorizationForm.tsx
@@ -1,13 +1,20 @@
 import { Input } from '@components/Input/Input';
 import { Button } from '@components/Button/Button';
-import React, { type FC, useCallback } from 'react';
-import { useFormFields } from '../../hooks/useFormFields';
+import React, { BaseSyntheticEvent, type FC, useCallback, useState } from 'react';
+import { useFormFields } from './hooks/useFormFields';
 import { toast } from 'react-toastify';
 import { login } from '@store/user/thunks';
 import styles from './AuthorizationForm.module.scss';
 import { useAppDispatch } from '@store/hooks/useAppDispatch';
+import { PasswordInput } from '@features/authorization/components/PasswordInput/PasswordInput';
+import { Panel } from '@components/Panel/Panel';
+import classNames from 'classnames';
 
-export const AuthorizationForm: FC = () => {
+interface IAuthorizationForm {
+	className?: string;
+}
+
+export const AuthorizationForm: FC<IAuthorizationForm> = ({ className }) => {
 	const formState = useFormFields();
 	const dispatch = useAppDispatch();
 
@@ -32,20 +39,32 @@ export const AuthorizationForm: FC = () => {
 	);
 
 	return (
-		<>
-			<div className={styles.wrapper}>
-				<form onSubmit={handleSubmit}>
-					<label htmlFor={'login_field'}>Email address</label>
-					<Input size={'size-l'} id={'login_field'} {...formState.email} />
+		<Panel className={classNames(styles.wrapper, className)}>
+			<form onSubmit={handleSubmit}>
+				<label className={styles.label} htmlFor={'login_field'}>
+					Email address
+				</label>
+				<Input
+					className={styles.input}
+					size={'size-l'}
+					id={'login_field'}
+					{...formState.email}
+				/>
 
-					<label htmlFor={'password_field'}>Password</label>
-					<Input size={'size-l'} id={'password_field'} {...formState.password} />
+				<label className={styles.label} htmlFor={'password_field'}>
+					Password
+				</label>
+				<PasswordInput
+					className={styles.input}
+					size={'size-l'}
+					id={'password_field'}
+					{...formState.password}
+				/>
 
-					<Button size={'size-l'} color={'primary'}>
-						Login
-					</Button>
-				</form>
-			</div>
-		</>
+				<Button size={'size-l'} color={'primary'} type={'submit'}>
+					Login
+				</Button>
+			</form>
+		</Panel>
 	);
 };
