cc4bcdc9470129215d49c07bf44f24d871d25b87
Temka Artemka <yandex949@gmail.com>
2023-03-10 00:21:37 +0300

    fix: removed credntials header from axios instance

--- src/axios/customAxios.ts
+++ src/axios/customAxios.ts
@@ -5,5 +5,4 @@ export const serverURL = 'https://zavod-backend.vercel.app';
 const RESTAPI_URL = serverURL + '/api';
 export const serverAPI = axios.create({
 	baseURL: RESTAPI_URL,
-	withCredentials: true,
 });
