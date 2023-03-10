8bf1e482603cac131319fa96811507bfd998105f
WhiteAzer <0202timur2020@gmail.com>
2023-03-10 12:37:11 +0300

    feat: added basic structure of Post

--- src/features/basic-components/components/Post/Post.tsx
+++ src/features/basic-components/components/Post/Post.tsx
@@ -1,22 +1,57 @@
 import { FC } from 'react';
 import styles from './Post.module.scss';
 import classNames from 'classnames';
-import { Comment, EntryType, ID, PartialUser, Post as TPost, PropsWithClass } from '@/types';
 import { Panel } from '@/components';
 import { UserStripe } from '@features/basic-components';
-import { user } from '@/data';
 import { getPostAt } from './helpers/getPostAt';
+import { IPost, IUser } from '@/types/data-types';
+import { PropsWithClass } from '@/types/runtime-types';
+import { Icon20Like as LikeIcon } from '@vkontakte/icons';
+import { Icon24Comment as CommentIcon } from '@vkontakte/icons';
+
+const mockUser: IUser = {
+	id: '228',
+	firstname: 'string',
+	lastname: 'string',
+	username: 'string',
+	gender: 'male',
+	avatar: null,
+	entries: null,
+	likes: null,
+	roles: null,
+	friends: null,
+	following: null,
+	followers: null,
+	notifications: null,
+};
 
 type Props = PropsWithClass & {
-	post: TPost;
+	post: IPost;
 };
 
 export const Post: FC<Props> = ({ post, className }) => {
 	return (
 		<Panel>
-			<UserStripe size={'size-s'} user={user}>
+			<UserStripe size={'size-s'} user={mockUser} className={styles.userStripe}>
 				{getPostAt(+post.createdAt)}
 			</UserStripe>
+			<div className={styles.text}>{post.content.text}</div>
+			<div className={styles.imgs}>
+				{!!post.content.images.length &&
+					post.content.images.map((el: string) => (
+						<img src={el} className={styles.img} key={el} />
+					))}
+			</div>
+			<div className={styles.actionBtns}>
+				<div className={styles.actionBtns_item}>
+					<span>{post.likes}</span>
+					<LikeIcon fill={'#C4CCD6'} />
+				</div>
+				<div className={styles.actionBtns_item}>
+					<span>{post.comments.length}</span>
+					<CommentIcon fill={'#C4CCD6'} />
+				</div>
+			</div>
 		</Panel>
 	);
 };
