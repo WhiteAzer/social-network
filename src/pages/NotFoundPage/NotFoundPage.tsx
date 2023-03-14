735dc60755fa89a6497df54b409e505cc1534089
Temka Artemka <yandex949@gmail.com>
2023-03-14 16:31:03 +0300

    feat: NotFoundPage differs for authed\not authued user

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,16 +1,29 @@
 import styles from './NotFoundPage.module.scss';
 import { FC } from 'react';
 import { Panel } from '@/components';
+import { useSelector } from 'react-redux';
+import { authorizedUserSelector } from '../../store/slices/userSlice/selectors';
+import { Link, Navigate } from 'react-router-dom';
 
 export const NotFoundPage: FC = () => {
+	const { status } = useSelector(authorizedUserSelector);
+
+	if (status !== 'succeed') {
+		return <Navigate to={'/auth/login'} />;
+	}
+
 	return (
 		<div className={styles.page}>
-			<Panel>
+			<Panel className={styles.wrapper}>
 				<h1>
 					404
 					<br />
 					Not found
 				</h1>
+
+				<p>
+					<Link to={'/home'}>Back to Home Page</Link>
+				</p>
 			</Panel>
 		</div>
 	);
