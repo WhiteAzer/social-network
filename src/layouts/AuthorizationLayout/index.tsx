44f6d000509247cc4a9bc1c60c88f73deb3da1d6
Temka Artemka <yandex949@gmail.com>
2023-03-14 16:29:56 +0300

    fix: AuthorizationLayout prevented routing

--- src/layouts/AuthorizationLayout/index.tsx
+++ src/layouts/AuthorizationLayout/index.tsx
@@ -1,20 +1,17 @@
-import React, { useEffect } from 'react';
+import React, { FC, PropsWithChildren, useEffect } from 'react';
 import { Outlet, useNavigate } from 'react-router-dom';
 import { useAppDispatch } from '../../store/hooks/useAppDispatch';
 import { useAppSelector } from '../../store/hooks/useAppSelector';
 import { authorizedUserSelector } from '../../store/slices/userSlice/selectors';
 import { loginByCookies } from '../../store/slices/userSlice/thunks';
 
-export const AuthorizationLayout = () => {
-	const { status, user } = useAppSelector(authorizedUserSelector);
+export const AuthorizationLayout: FC = () => {
+	const { status } = useAppSelector(authorizedUserSelector);
 	const dispatch = useAppDispatch();
-	const navigate = useNavigate();
 
 	useEffect(() => {
 		if (status === 'idle') {
 			dispatch(loginByCookies());
-		} else if (status === 'succeed') {
-			navigate('/home');
 		}
 	}, [status]);
 
